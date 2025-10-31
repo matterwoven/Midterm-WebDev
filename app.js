3cf0e716681af9fbae79d874a567394ebd8ceea8
Matthew Vargas <matthewvargas2013@gmail.com>
2025-10-30 21:06:00 -0700

    UpdatedPort

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3125;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
